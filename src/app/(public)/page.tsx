8dd1fd9204c10cf08dfaa3b93c7eba687e92cdae
binamralamsal <binamralamsal@gmail.com>
2025-02-17 23:42:32 +0545

    added fashion image

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -73,7 +73,7 @@ export default async function Home() {
           href="/products?categories=adventure"
           className="group relative grid place-items-center gap-2 py-40 transition md:py-48 lg:py-52"
         >
-          <div className="absolute bottom-0 left-0 right-0 top-0 -z-10 bg-[url('/adventure.jpg')] bg-cover bg-center brightness-90 transition duration-300 group-hover:brightness-[0.8]"></div>
+          <div className="absolute bottom-0 left-0 right-0 top-0 -z-10 bg-[url('/adventure.jpg')] bg-cover bg-bottom brightness-90 transition duration-300 group-hover:brightness-[0.8]"></div>
           <div className="text-3xl font-bold text-white md:text-4xl lg:text-5xl">
             Adventure
           </div>
